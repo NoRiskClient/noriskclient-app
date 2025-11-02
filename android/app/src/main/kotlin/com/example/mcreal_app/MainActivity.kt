98f1f48539a29720b1258adb6e6ca6a766fe2317
JHubi1 <me@jhubi1.com>
2025-11-02 15:18:43 +0100

    Updated Android, removed other platforms
    Except for iOS

--- android/app/src/main/kotlin/com/example/mcreal_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package gg.norisk.mcreal
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/gg/norisk/noriskclient/MainActivity.kt
+++ android/app/src/main/kotlin/gg/norisk/noriskclient/MainActivity.kt
@@ -2,4 +2,4 @@ package gg.norisk.noriskclient
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
